12310f1f475ae384cd7c3523030442dc2d73d0ad
PATAWEE <patawee14@hotmail.com>
2019-08-01 15:49:41 +0700

    update data

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+// server.js
 //Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/omss'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/omss/index.html'));
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+console.log('port' , port);
+app.set(port);
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running…'));
